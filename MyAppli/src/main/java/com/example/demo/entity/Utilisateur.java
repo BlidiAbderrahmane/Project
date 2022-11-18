83a8b0f9276e1fb4b37ad54d6b90522ce8981c69
medAmineCH37 <79566268+medAmineCH37@users.noreply.github.com>
2022-11-18 14:58:13 +0100

    Update Utilisateur.java

--- MyAppli/src/main/java/com/example/demo/entity/Utilisateur.java
+++ MyAppli/src/main/java/com/example/demo/entity/Utilisateur.java
@@ -1,5 +1,46 @@
 package com.example.demo.entity;
 
+<<<<<<< Updated upstream
 public class Utilisateur {
 
 }
+=======
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name="T_Utilisateur")
+public class Utilisateur implements Serializable{
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_util;
+
+	@Column(name="nom")
+	private String nom_util;
+
+	@Column(name="prenom")
+	private String prenom_util;
+	
+	@Column(name="adresse")
+	private String adresse_util;
+	
+	@Column(name="email")
+	private String email_util;
+	
+	@Column(name="codePostal")
+	private int codePostal_util;
+	
+	@JsonFormat(pattern="dd-MM-YYYY")
+	@Column(name="dateNaissance")
+	private Date dateNaissance_util;
+
+}
+>>>>>>> Stashed changes
