b81534b51ae45822d62cce044c8fffeb68d5d6d9
medAmineCH37 <79566268+medAmineCH37@users.noreply.github.com>
2022-11-28 12:01:21 +0100

    repositories

---
+++ MyAppli/src/main/java/com/example/demo/repository/LaboratoireRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Laboratoire;
+
+public interface LaboratoireRepository extends JpaRepository <Laboratoire,Long>{
+
+}
